5634dde66083966b0021c8616ed2ec02ee972d5c
Dima Prokopiv <dimaprokopiv@gmail.com>
2019-06-02 19:06:17 +0300

    added task 3 from HW1

---
+++ Homework1_task3.py
@@ -0,0 +1,13 @@
+value_int =10
+value_float =8.4
+value_str="No"
+big_int=value_int *3.5
+value_float=value_float-1
+value_int /value_float
+big_int/value_float
+value_str= value_str +"NO" + 3*"Yes"
+
+print("Value_str:",value_str)
+print("big_int:",big_int)
+print("value_float:",value_float)
+print("value_int:",value_int)

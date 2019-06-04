362dd36ff55cd475d6b84868194e7b21b718f39f
Svjatoslav Nabytovych <svjatnab@gmail.com>
2019-06-04 17:37:44 +0300

    add homework1

---
+++ home_work.py
@@ -0,0 +1,23 @@
+print("What is your name?")
+name = input()
+print('your name is ' + name)
+
+print("how old are you")
+age = input()
+print('your age is ' + age)
+
+print("Where are you live")
+sity = input()
+print('You live in  ' + sity)
+
+print("input a")
+a = input()
+
+print("input b")
+b = input()
+print("a+b  ="+ str(int(a)+int(b)))
+print("a-b  ="+ str(int(a)-int(b)))
+print("a/b  ="+ str(int(a)/int(b)))
+print("a*b  ="+ str(int(a)*int(b)))
+
+

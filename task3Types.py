50e82aeebec01fba94c9d78e97e00b953f2d4a7f
merito <meritoid@gmail.com>
2019-06-05 11:33:37 +0300

    task3 from Types

---
+++ task3Types.py
@@ -0,0 +1,14 @@
+x = 50
+y = 13
+
+print("Before x value is: %d\nBefore y value is: %d\n" % (x,y))
+
+x=x+y # now x is equal 63
+y=x-y # now y is equel 50
+x=x-y # now x is 13
+
+print("After x value is: %d\nAfter y value is: %d" % (x,y))
+
+
+
+

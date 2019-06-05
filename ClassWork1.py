1912f7838ac73d42010ff13dae17fcbefc2264a2
merito <meritoid@gmail.com>
2019-06-05 11:42:19 +0300

    classwork

---
+++ ClassWork1.py
@@ -0,0 +1,10 @@
+x = int(input('Input first number: '))
+y = int(input ('Input second number: '))
+
+if x>y :
+    print("First number is greater than second number")
+elif x==y :
+    print("Two numbers are equal")
+elif y>x:
+    print("Second number is greater than first number")
+

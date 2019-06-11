5e3f1af04588489439ab0734e4e0ac2955f996fd
Oleksii Bilyi <bilyi.oleksii@gmail.com>
2019-06-11 22:40:31 +0300

    adding hw1.py

---
+++ HW1.py
@@ -0,0 +1,11 @@
+value_int = 10
+value_float = 8.4
+value_str = "No"
+
+big_int = value_int * 3.5
+value_float += 1
+value_int / value_float
+big_int / value_float
+value_str += "No" + "Yes" * 3
+
+print(big_int, value_float, value_str)
